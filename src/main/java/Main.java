444bdc8f1eacc71ceba4a693639663ba74fc0b2f
little_grees <735834365@qq.com>
2024-02-19 15:40:59 +0800

    pass 2st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,21 +17,20 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
+      System.out.println("客户端连接："+clientSocket.getPort());
 
       try(
               InputStream inputStream = clientSocket.getInputStream();
               final OutputStream outputStream = clientSocket.getOutputStream()
       ) {
-        byte[] bs = new byte[1024];
-        int read;
-        while ((read = inputStream.read(bs)) > 0) {
-          byte[] bytes = Arrays.copyOf(bs, read);
-          final String content = new String(bytes);
-          String response = "+PONG\r\n";
-          if (content.equalsIgnoreCase("PING")) {
-            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-            outputStream.flush();
-          }
+      byte[] bs = new byte[1024];
+      int read;
+      if ((read = inputStream.read(bs)) >= 0) {
+        byte[] bytes = Arrays.copyOf(bs, read);
+        final String content = new String(bytes);
+        String response = "+PONG\r\n";
+          outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+          outputStream.flush();
         }
       }
 
@@ -41,6 +40,9 @@ public class Main {
       try {
         if (clientSocket != null) {
           clientSocket.close();
+        }
+        if (serverSocket != null) {
+          serverSocket.close();
         }
       } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
