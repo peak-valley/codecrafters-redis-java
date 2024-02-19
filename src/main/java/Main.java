393baa3435cf0d6c829646d9ffbe0b6e74d9d8c5
little_grees <735834365@qq.com>
2024-02-19 14:52:24 +0800

    pass 2st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args){
@@ -12,6 +17,22 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
+
+      try(
+              InputStream inputStream = clientSocket.getInputStream();
+              final OutputStream outputStream = clientSocket.getOutputStream()
+      ) {
+        byte[] bs = new byte[1024];
+
+        final int read = inputStream.read(bs);
+        byte[] bytes = Arrays.copyOf(bs, read);
+        final String content = new String(bytes);
+        String response = "PONG\r\n";
+        if (content.equals("PING")) {
+          outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+          outputStream.flush();
+        }
+      }
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
