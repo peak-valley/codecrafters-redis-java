4d72123d0c6981086dd03145adbd528401109c65
little_grees <735834365@qq.com>
2024-02-20 21:42:54 +0800

    replication stage 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,9 +13,12 @@ public class Main {
   public static void main(String[] args){
     final ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-
-    ServerSocket serverSocket = null;
+    Integer p = resolve(args);
     int port = 6379;
+    if (p != null) {
+      port = p;
+    }
+    ServerSocket serverSocket = null;
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
@@ -38,5 +41,18 @@ public class Main {
         System.out.println("IOException: " + e.getMessage());
       }
     }
+  }
+
+  public static Integer resolve(String[] args) {
+    if (args.length == 0) {
+      return null;
+    }
+    String port = args[0];
+    if (port == null || port.length() < 8) {
+      return null;
+    }
+
+    port = port.replaceFirst("--port=", "");
+    return Integer.parseInt(port);
   }
 }
