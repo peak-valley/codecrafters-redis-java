e0a4a41873d1adc7e930b0ffa6f490e75c350beb
little_grees <735834365@qq.com>
2024-02-25 22:46:53 +0800

    replication stage 14 ACKs with no commands

--- src/main/java/com/zyf/commands/AbstractCommand.java
+++ src/main/java/com/zyf/commands/AbstractCommand.java
@@ -1,5 +1,12 @@
 package com.zyf.commands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.zyf.Constant.Constants._R_N;
+
 public abstract class AbstractCommand implements Command {
     public byte[] buildBulkResponse(String content) {
         int length = content.length();
@@ -13,5 +20,25 @@ public abstract class AbstractCommand implements Command {
 
     public byte[] buildSimpleStrResponse(String content) {
         return ("+" + content + "\r\n").getBytes();
+    }
+
+    public byte[] buildArraysResponse(List<Object> data) {
+
+        StringBuilder command = new StringBuilder("*" + data.size() + _R_N);
+        for (Object o : data) {
+            String o1 = new String((byte[]) o);
+            command.append("$").append(o1.length()).append(_R_N).append(o1).append(_R_N);
+        }
+        System.out.println("build command ->" + command);
+        return command.toString().getBytes();
+    }
+
+    public static void main(String[] args) {
+        ReplConf replConf = new ReplConf();
+        List<Object> objects = new ArrayList<>();
+        objects.add("REPLCONF".getBytes());
+        objects.add("GETACK".getBytes());
+        objects.add("*".getBytes());
+        System.out.println(replConf.buildArraysResponse(objects));
     }
 }
--- src/main/java/com/zyf/commands/ReplConf.java
+++ src/main/java/com/zyf/commands/ReplConf.java
@@ -5,7 +5,13 @@ import java.util.List;
 public class ReplConf extends AbstractCommand {
     @Override
     public byte[] execute(List<Object> content) {
-
+        if (content.size() < 2) {
+            return buildSimpleStrResponse("OK");
+        }
+        String param1 = new String((byte[]) content.get(1));
+        if ("GETACK".equals(param1)) {
+            return buildArraysResponse(List.of("REPLCONF", "ACK", 0));
+        }
         return buildSimpleStrResponse("OK");
     }
 }
