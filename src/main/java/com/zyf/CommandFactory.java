3fa09da97eed39e0694ae4467da4c33d258deb7e
little_grees <735834365@qq.com>
2024-02-27 23:29:58 +0800

    rdb stage 1

--- src/main/java/com/zyf/CommandFactory.java
+++ src/main/java/com/zyf/CommandFactory.java
@@ -21,7 +21,7 @@ public class CommandFactory {
         commandCache.put(CommandEnum.PSYNC.getName(), new Psync());
         commandCache.put(CommandEnum.FULLRESYNC.getName(), new FullResync());
         commandCache.put(CommandEnum.WAIT.getName(), new Wait());
-        commandCache.put(CommandEnum.CONFIG.getName(), new CONFIG());
+        commandCache.put(CommandEnum.CONFIG.getName(), new Config());
     }
 
     public byte[] execute(String command, List<Object> content) {
---
+++ src/main/java/com/zyf/commands/Config.java
@@ -0,0 +1,29 @@
+package com.zyf.commands;
+
+import com.zyf.Constant.Constants;
+import com.zyf.collect.SimpleKVCache;
+import com.zyf.rdb.RDBCache;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Config extends AbstractCommand {
+    @Override
+    public byte[] execute(List<Object> content) {
+        if (content.size() <= 2) {
+            return null;
+        }
+
+        String param1 = new String((byte[]) content.get(1));
+        String param2 = new String((byte[]) content.get(2));
+        if ("GET".equals(param1)) {
+            String v = RDBCache.get(param2);
+            List<Object> list = new ArrayList<>();
+            Collections.addAll(list, param1, v);
+            return buildArraysResponse(list);
+        }
+
+        return new byte[0];
+    }
+}
