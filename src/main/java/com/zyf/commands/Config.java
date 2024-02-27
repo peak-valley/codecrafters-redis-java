dc3d0113b7f9222f6b21455f4a0595940e459052
little_grees <735834365@qq.com>
2024-02-27 23:48:33 +0800

    rdb stage 1

--- src/main/java/com/zyf/commands/Config.java
+++ src/main/java/com/zyf/commands/Config.java
@@ -17,10 +17,11 @@ public class Config extends AbstractCommand {
 
         String param1 = new String((byte[]) content.get(1));
         String param2 = new String((byte[]) content.get(2));
-        System.out.println("CONFIG -> " + param2 + ":" + param2);
+        System.out.println("CONFIG -> " + param1 + ":" + param2);
         if ("GET".equals(param1)) {
             String v = RDBCache.get(param2);
             List<Object> list = new ArrayList<>();
+            System.out.println("CONFIG value is" +v);
             Collections.addAll(list, param1, v);
             return buildArraysResponse(list);
         }
