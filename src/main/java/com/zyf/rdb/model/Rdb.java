73ce44b7d3d5d798b75c9be048f68d056d49708e
little_grees <735834365@qq.com>
2024-02-28 16:46:03 +0800

    RDB > Stage 2

--- src/main/java/com/zyf/rdb/model/Rdb.java
+++ src/main/java/com/zyf/rdb/model/Rdb.java
@@ -12,7 +12,7 @@ public record Rdb(
     public void init() {
         for (RdbDbInfo dbInfo : rdbDbInfos) {
             dbInfo.rdbPairs().forEach(rdbPair -> SimpleKVCache.put(rdbPair.key(), new KVString(rdbPair.key(), rdbPair.value())));
-//            dbInfo.rdbExpirePairs().forEach(rdbExpirePair -> RedisRepository.setWithExpireTimestamp(rdbExpirePair.key(), rdbExpirePair.value(), rdbExpirePair.expireTime()));
+            dbInfo.rdbExpirePairs().forEach(rdbExpirePair -> SimpleKVCache.put(rdbExpirePair.key(), new KVString(rdbExpirePair.key(), rdbExpirePair.value(), rdbExpirePair.expireTime())));
         }
     }
 }
